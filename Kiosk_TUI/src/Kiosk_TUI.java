9a1419d209098c10917704b1334fa85ac7cc9138
suk-6 <me@suk.kr>
2023-06-12 22:05:03 +0900

    230612

---
+++ Kiosk_TUI/src/Kiosk_TUI.java
@@ -0,0 +1,15 @@
+public class Kiosk_TUI {
+    public static void main(String[] args) throws Exception {
+        int a = 9;
+        boolean b = true;
+        char c = 'd';
+        double d = 3.14;
+        String e = "조용히 좀 해라";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
