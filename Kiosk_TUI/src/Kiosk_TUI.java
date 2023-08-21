8354cac7493ec842964683cbf393083a84da4775
suk-6 <me@suk.kr>
2023-08-22 08:12:05 +0900

    Class: 0821

--- Kiosk_TUI/src/Kiosk_TUI.java
+++ Kiosk_TUI/src/Kiosk_TUI.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kiosk_TUI {
 
     String helloMsg = "Hello World!";
@@ -12,12 +14,32 @@ public class Kiosk_TUI {
         myKiosk.menus[3] = new Coffee("카페모카", 4500, false, false, "콜롬비아");
         myKiosk.menus[4] = new Coffee("카라멜마끼야또", 3300, true, false, "멕시코");
 
-        for (int i = 0; i < myKiosk.menus.length; i++) {
-            System.out.println(myKiosk.menus[i]);
+        myKiosk.showMenus();
+
+        System.out.println("\n원하는 메뉴를 입력해주세요(1~" + myKiosk.menus.length + "): ");
+        Scanner myScanner = new Scanner(System.in);
+        String input = myScanner.nextLine();
+        myScanner.close();
+
+        try {
+            int menuNum = Integer.parseInt(input);
+            if (menuNum < 1 || menuNum > myKiosk.menus.length) {
+                System.out.println("잘못된 입력입니다.");
+                return;
+            }
+            System.out.println("선택하신 메뉴는 [" + myKiosk.menus[menuNum - 1] + "] 입니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     int pay() {
         return 500;
+    }
+
+    void showMenus() {
+        for (int i = 0; i < menus.length; i++) {
+            System.out.println(menus[i]);
+        }
     }
 }
