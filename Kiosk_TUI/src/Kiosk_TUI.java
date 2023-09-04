88840ccb0efcfc9db66a5c9b1a23d4fafdcc7612
suk-6 <me@suk.kr>
2023-09-04 19:24:33 +0900

    Class: 0904

--- Kiosk_TUI/src/Kiosk_TUI.java
+++ Kiosk_TUI/src/Kiosk_TUI.java
@@ -7,41 +7,40 @@ public class Kiosk_TUI {
     Menu[] menus = new Menu[5];
 
     public static void main(String[] args) {
-        Kiosk_TUI myKiosk = new Kiosk_TUI();
-        myKiosk.menus[0] = new Menu("아아", 3000);
-        myKiosk.menus[1] = new Menu("뜨아", 3300);
-        myKiosk.menus[2] = new Beverage("레몬에이드", 4000, true);
-        myKiosk.menus[3] = new Coffee("카페모카", 4500, false, false, "콜롬비아");
-        myKiosk.menus[4] = new Coffee("카라멜마끼야또", 3300, true, false, "멕시코");
+        while (true) {
+            Kiosk_TUI myKiosk = new Kiosk_TUI();
+            myKiosk.menus[0] = new Menu("아아", 3000);
+            myKiosk.menus[1] = new Menu("뜨아", 3300);
+            myKiosk.menus[2] = new Beverage("레몬에이드", 4000, true);
+            myKiosk.menus[3] = new Coffee("카페모카", 4500, false, false, "콜롬비아");
+            myKiosk.menus[4] = new Coffee("카라멜마끼야또", 3300, true, false, "멕시코");
 
-        myKiosk.showMenus();
+            myKiosk.showMenus();
 
-        System.out.println("\n원하는 메뉴를 입력해주세요(1~" + myKiosk.menus.length + "): ");
-        Scanner myScanner = new Scanner(System.in);
-        String input = myScanner.nextLine();
+            System.out.println("\n원하는 메뉴를 입력해주세요(1~" + myKiosk.menus.length + "): ");
+            Scanner myScanner = new Scanner(System.in);
+            String input = myScanner.nextLine();
 
-        int selectMenuPrice = 0;
+            int selectMenuPrice = 0;
 
-        try {
-            int menuNum = Integer.parseInt(input);
-            if (menuNum < 1 || menuNum > myKiosk.menus.length) {
-                System.out.println("잘못된 입력입니다.");
-                myScanner.close();
-                return;
+            try {
+                int menuNum = Integer.parseInt(input);
+                if (menuNum < 1 || menuNum > myKiosk.menus.length) {
+                    throw new Exception("메뉴 번호가 잘못되었습니다.");
+                }
+
+                selectMenuPrice = myKiosk.menus[menuNum - 1].price;
+                System.out.println("선택하신 메뉴는 [" + myKiosk.menus[menuNum - 1] + "] 입니다.\n");
+                System.out.println("결제를 진행합니다.");
+                System.out.println("현금 입력: ");
+                input = myScanner.nextLine();
+
+                int cash = Integer.parseInt(input);
+
+                myKiosk.pay(selectMenuPrice, cash);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            selectMenuPrice = myKiosk.menus[menuNum - 1].price;
-            System.out.println("선택하신 메뉴는 [" + myKiosk.menus[menuNum - 1] + "] 입니다.\n");
-            System.out.println("결제를 진행합니다.");
-            System.out.println("현금 입력: ");
-            input = myScanner.nextLine();
-            myScanner.close();
-
-            int cash = Integer.parseInt(input);
-
-            myKiosk.pay(selectMenuPrice, cash);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
